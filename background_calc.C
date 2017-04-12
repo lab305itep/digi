11fbe1ade6aaf6fb5528c6cd324eaf71eccb2d37
Igor Alekseev <Igor.Alekseev@itep.ru>
2017-04-13 01:30:55 +0300

    Small changes

--- background_calc.C
+++ background_calc.C
@@ -21,11 +21,15 @@ void background_calc(const char *fname = "background_plots.root", int run_first 
 	TCut cX("PositronX[0] < 0 || (PositronX[0] > 2 && PositronX[0] < 94)");
 	TCut cY("PositronX[1] < 0 || (PositronX[1] > 2 && PositronX[1] < 94)");
 	TCut cZ("PositronX[2] > 3.5 && PositronX[2] < 95.5");
-	TCut cR2("PositronX[0] >= 0 && PositronX[1] >= 0 && NeutronX[0] >= 0 && NeutronX[1] >= 0");
+	TCut cRXY("PositronX[0] >= 0 && PositronX[1] >= 0 && NeutronX[0] >= 0 && NeutronX[1] >= 0");
 	TCut c20("gtDiff > 2");
-        TCut cGamma("AnnihilationEnergy < 1.5 && AnnihilationGammas < 9");
+        TCut cGamma("AnnihilationEnergy < 1.8 && AnnihilationGammas <= 10");
         TCut cPe("PositronEnergy > 1");
-        TCut cR("Distance < 60 && fabs(DistanceZ) < 40");
+        TCut cR1("Distance < 45");
+        TCut cR2("Distance < 55");
+        TCut cRZ("fabs(DistanceZ) < 40");
+        TCut cR = cR2 && (cRXY || cR1) && cRZ;
+        TCut cN("NeutronEnergy > 3.5");
         TCut ct;
 
 	TFile *fRoot = new TFile(fname, "RECREATE");
@@ -84,25 +88,25 @@ void background_calc(const char *fname = "background_plots.root", int run_first 
 	hp->SetFile(fRoot);
 
 	for (j=0; j<2; j++) {
-		ct = cIso && cX && cY && cZ && cR && cPe && cGamma;
+		ct = cIso && cX && cY && cZ && cR && cPe && cGamma && cN;
 		hp->Project(h[0][j], "gtDiff", (j) ? (ct && !cVeto) : (ct && cVeto));
-		ct = cIso && cX && cY && cZ && cPe && cGamma && !cR2;
+		ct = cIso && cX && cY && cZ && cPe && cGamma && cN && !cRXY;
 		hp->Project(h[1][j], "Distance", (j) ? (ct && !cVeto) : (ct && cVeto));
-		ct = cIso && cX && cY && cZ && cPe && cGamma && cR2;
+		ct = cIso && cX && cY && cZ && cPe && cGamma && cN && cRXY;
 		hp->Project(h[2][j], "Distance", (j) ? (ct && !cVeto) : (ct && cVeto));
-		ct = cIso && cX && cY && cZ && cPe && cGamma;
+		ct = cIso && cX && cY && cZ && cPe && cGamma && cN;
 		hp->Project(h[3][j], "DistanceZ", (j) ? (ct && !cVeto) : (ct && cVeto));
-		ct = cIso && cY && cZ && cR && cPe && cGamma && "PositronX[0] >= 0";
+		ct = cIso && cY && cZ && cR && cPe && cGamma && cN && "PositronX[0] >= 0";
 		hp->Project(h[4][j], "PositronX[0] + 2.0", (j) ? (ct && !cVeto) : (ct && cVeto));
-		ct = cIso && cX && cZ && cR && cPe && cGamma && "PositronX[1] >= 0";
+		ct = cIso && cX && cZ && cR && cPe && cGamma && cN && "PositronX[1] >= 0";
 		hp->Project(h[5][j], "PositronX[1] + 2.0", (j) ? (ct && !cVeto) : (ct && cVeto));
-		ct = cIso && cX && cY && cR && cPe && cGamma && "PositronX[2] >= 0";
+		ct = cIso && cX && cY && cR && cPe && cGamma && cN && "PositronX[2] >= 0";
 		hp->Project(h[6][j], "PositronX[2] + 0.5", (j) ? (ct && !cVeto) : (ct && cVeto));
-		ct = cIso && cY && cZ && cR && cPe && cGamma && "NeutronX[0] >= 0";
+		ct = cIso && cY && cZ && cR && cPe && cGamma && cN && "NeutronX[0] >= 0";
 		hp->Project(h[7][j], "NeutronX[0] + 2.0", (j) ? (ct && !cVeto) : (ct && cVeto));
-		ct = cIso && cX && cZ && cR && cPe && cGamma && "NeutronX[1] >= 0";
+		ct = cIso && cX && cZ && cR && cPe && cGamma && cN && "NeutronX[1] >= 0";
 		hp->Project(h[8][j], "NeutronX[1] + 2.0", (j) ? (ct && !cVeto) : (ct && cVeto));
-		ct = cIso && cX && cY && cR && cPe && cGamma && "NeutronX[2] >= 0";
+		ct = cIso && cX && cY && cR && cPe && cGamma && cN && "NeutronX[2] >= 0";
 		hp->Project(h[9][j], "NeutronX[2] + 0.5", (j) ? (ct && !cVeto) : (ct && cVeto));
 		ct = cIso && cX && cY && cZ && cR && cPe && cGamma;
 		hp->Project(h[10][j], "NeutronEnergy", (j) ? (ct && !cVeto) : (ct && cVeto));
@@ -110,15 +114,15 @@ void background_calc(const char *fname = "background_plots.root", int run_first 
 		hp->Project(h[11][j], "NeutronHits", (j) ? (ct && !cVeto) : (ct && cVeto));
 		ct = cIso && cX && cY && cZ && cR && cPe && cGamma;
 		hp->Project(h[12][j], "NeutronRadius", (j) ? (ct && !cVeto) : (ct && cVeto));
-		ct = cIso && cX && cY && cZ && cR && cPe && cGamma;
+		ct = cIso && cX && cY && cZ && cR && cPe && cGamma && cN;
 		hp->Project(h[13][j], "PositronHits", (j) ? (ct && !cVeto) : (ct && cVeto));
-		ct = cIso && cX && cY && cZ && cR && cPe && cGamma;
+		ct = cIso && cX && cY && cZ && cR && cPe && cGamma && cN;
 		hp->Project(h[14][j], "MaxHitEnergy", (j) ? (ct && !cVeto) : (ct && cVeto));
-		ct = cIso && cX && cY && cZ && cR && cPe;
+		ct = cIso && cX && cY && cZ && cR && cPe && cN;
 		hp->Project(h[15][j], "AnnihilationGammas", (j) ? (ct && !cVeto) : (ct && cVeto));
-		ct = cIso && cX && cY && cZ && cR && cPe;
+		ct = cIso && cX && cY && cZ && cR && cPe && cN;
 		hp->Project(h[16][j], "AnnihilationEnergy", (j) ? (ct && !cVeto) : (ct && cVeto));
-		ct = cIso && cX && cY && cZ && cR && cPe && cGamma;
+		ct = cIso && cX && cY && cZ && cR && cPe && cGamma && cN;
 		hp->Project(h[17][j], "(PositronFlags[0] && 0x3FF0000) ? 1.5 : 0.5", (j) ? (ct && !cVeto) : (ct && cVeto));
 	}
 	
@@ -127,3 +131,160 @@ void background_calc(const char *fname = "background_plots.root", int run_first 
 	delete hp;
 	fRoot->Close();
 }
+
+
+void background_draw_all(const char *rootname = "background_plots.root")
+{
+	char strs[128];
+	char strl[1024];
+	const char titles[NHISTS][16] = {"gtDiff", "R1", "R2", "RZ", "PX", "PY", "PZ", "NX", "NY", "NZ", "NE", "NH", "NR", "PH", "PMaxE", "AH", "AE", "PF"};
+	const char suffix[3][10] = {"N-rand", "N-diff", "C-diff"};
+	const Color_t color[3] = {kGreen+2, kBlue, kRed};
+	const int marker[3] = {kFullStar, kFullCircle, kFullCross};
+	TH1D *h[NHISTS][3];
+	TH1D *hf[NHISTS][3];
+	int i, j, k, kl, ku;
+	double hMax;
+	int iMax;
+	double total, totale;
+	double frac, frace;
+	char pdfname[1024];
+	char *ptr;
+
+	strcpy(pdfname, rootname);
+	ptr = strstr(pdfname, ".root");
+	if (ptr) {
+		strcpy(ptr, ".pdf");
+	} else {
+		strcat(pdfname, ".pdf");
+	}
+	
+	gROOT->SetStyle("Plain");
+	gStyle->SetOptStat(0);
+	gStyle->SetOptFit(1);
+	
+	gStyle->SetTitleXSize(0.05);
+	gStyle->SetTitleYSize(0.05);
+	gStyle->SetLabelSize(0.05);
+	gStyle->SetPadLeftMargin(0.16);
+	gStyle->SetPadBottomMargin(0.16);
+	
+	TFile *fRoot = new TFile(rootname);
+	if (!fRoot->IsOpen()) {
+		printf("root-file not found  - run background_calc() first!\n");
+		return;
+	}
+	for (i=0; i<NHISTS; i++) {
+		for (j=0; j<3; j++) {
+			sprintf(strs, "h%s%s", titles[i], suffix[j]);
+			h[i][j] = (TH1D*) fRoot->Get(strs);
+			if (!h[i][j]) {
+				printf("%s not found  - rerun background_calc() to create all hists!\n", rootname);
+				fRoot->Close();
+				return;
+			}
+			h[i][j]->SetLineWidth(2);
+			h[i][j]->SetLineColor(color[j]);
+			h[i][j]->SetMarkerColor(color[j]);
+			h[i][j]->SetMarkerStyle(marker[j]);
+			h[i][j]->GetYaxis()->SetLabelSize(0.05);
+			h[i][j]->SetMinimum(0);
+			h[i][j]->GetYaxis()->SetTitle("");
+//			Fractions
+			sprintf(strs, "h%s%s-f", titles[i], suffix[j]);
+			hf[i][j] = (TH1D *) h[i][j]->Clone(strs);
+			hf[i][j]->Clear();
+			switch(i) {
+			case 0:		// from top
+			case 1:
+			case 2:
+			case 12:
+			case 15:
+			case 16:
+			case 17:
+				hf[i][j]->SetTitle("Cutting from top");
+				total = h[i][j]->IntegralAndError(1, hf[i][j]->GetNbinsX(), totale);
+				for (k=0; k<hf[i][j]->GetNbinsX(); k++) {
+					frac = h[i][j]->IntegralAndError(1, k+1, frace);
+					hf[i][j]->SetBinContent(k+1, frac/total);
+					hf[i][j]->SetBinError(k+1, (frac/total) * 
+						TMath::Sqrt((frace*frace/(frac*frac) + totale*totale/(total*total))));
+				}
+				break;
+			case 3:		// from both sides
+			case 4:
+			case 5:
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+				hf[i][j]->SetTitle("Cutting from both sides");
+				total = h[i][j]->IntegralAndError(1, hf[i][j]->GetNbinsX(), totale);
+				for (k=0; k<hf[i][j]->GetNbinsX(); k++) {
+					if (k+1 <= hf[i][j]->GetNbinsX()/2) {
+						kl = k+1;
+						ku = hf[i][j]->GetNbinsX() - k;
+					} else {
+						ku = k+1;
+						kl = hf[i][j]->GetNbinsX() - k;
+					}
+					frac = h[i][j]->IntegralAndError(kl, ku, frace);
+					hf[i][j]->SetBinContent(k+1, frac/total);
+					hf[i][j]->SetBinError(k+1, (frac/total) * 
+						TMath::Sqrt((frace*frace/(frac*frac) + totale*totale/(total*total))));
+				}
+				break;
+			case 10: 	// from bottom
+			case 11:
+			case 13:
+			case 14:
+				hf[i][j]->SetTitle("Cutting from bottom");
+				total = h[i][j]->IntegralAndError(1, hf[i][j]->GetNbinsX(), totale);
+				for (k=0; k<hf[i][j]->GetNbinsX(); k++) {
+					frac = h[i][j]->IntegralAndError(k+1, hf[i][j]->GetNbinsX(), frace);
+					hf[i][j]->SetBinContent(k+1, frac/total);
+					hf[i][j]->SetBinError(k+1, (frac/total) * 
+						TMath::Sqrt((frace*frace/(frac*frac) + totale*totale/(total*total))));
+				}
+				break;
+			}
+			hf[i][j]->SetMaximum(1.0);
+			hf[i][j]->SetMinimum(0.0);
+		}
+	}
+	
+	TCanvas *cv = new TCanvas("CV", "Background plots", 1200, 900);
+	TLegend *lg = new TLegend(0.7, 0.8, 0.95, 0.95);
+	lg->AddEntry(h[0][0], "Random", "LP");
+	lg->AddEntry(h[0][1], "Neutrino", "LP");
+	lg->AddEntry(h[0][2], "Cosmic", "LP");
+	
+	sprintf(strl, "%s[", pdfname);
+	cv->SaveAs(strl);
+	
+	for (i=0; i<NHISTS; i++) {
+		cv->Clear();
+		cv->Divide(2, 1);
+
+		cv->cd(1);
+		hMax = 0;
+		iMax = 0;
+		for (j=0; j<3; j++) if (h[i][j]->GetMaximum() > hMax) {
+			hMax = h[i][j]->GetMaximum();
+			iMax = j;
+		}
+		h[i][iMax]->Draw();
+		for (j=0; j<3; j++) if (j != iMax) h[i][j]->Draw("same");
+		lg->Draw();
+		
+		cv->cd(2);
+		for (j=0; j<3; j++) hf[i][j]->Draw((j) ? "same" : "");
+		
+		cv->SaveAs(pdfname);
+	}
+	
+	sprintf(strl, "%s]", pdfname);
+	cv->SaveAs(strl);
+	fRoot->Close();
+}
+
--- draw_R2.C
+++ draw_R2.C
@@ -1,9 +1,11 @@
 void draw_R2(void)
 {
+//		Original data
 	double r[9] = {10.395, 11.395, 12.395, 10.7, 11.7, 12.7, 11.005, 12.005, 13.005};
 	double er[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 	double cnt[9] = {1878, 1577, 1361, 1851, 1572, 1321, 1585, 1341, 1140};
 	double ecnt[9] = {14, 17, 15, 14, 17, 14, 12, 15, 12};
+	
 	TGraphErrors *gr;
 	TH1D *hst;
 	double par[3];
@@ -14,18 +16,21 @@ void draw_R2(void)
 	gStyle->SetOptFit(0);
 	TF1 *fR2 = new TF1("fR2", "[0]/(x * x)");
 	fR2->SetParNames("Const.");
+//		Fit 3 individual distributions
 	for (i=0; i<3; i++) {
 		gr = new TGraphErrors(3, &r[3*i], &cnt[3*i], &er[3*i], &ecnt[3*i]);
 		fR2->SetParameter(0, 200000);
 		gr->Fit(fR2);
 		par[i] = fR2->GetParameter(0);
 	}
+//		Renorm top and bottom sections to the middle
 	for (i=0; i<3; i++) {
 		cnt[i] *= par[1] / par[0];
 		cnt[6+i] *= par[1] / par[2];
 		ecnt[i] *= par[1] / par[0];
 		ecnt[6+i] *= par[1] / par[2];
 	}
+	for (i=0; i<9; i++) printf("L = %8.3f    CNT = %6.1f +- %4.1f\n", r[i], cnt[i], ecnt[i]);
 	gr = new TGraphErrors(9, r, cnt, er, ecnt);
 	gr->SetLineColor(kBlue);
 	gr->SetLineWidth(4);
@@ -37,11 +42,15 @@ void draw_R2(void)
 	hst = new TH1D("H", ";Distance to reactor core center, m;Events per day", 35, 10, 13.5);
 	hst->SetMinimum(1200);
 	hst->SetMaximum(2100);
-	hst->GetXaxis()->SetLabelSize(0.08);
-	hst->GetYaxis()->SetLabelSize(0.08);
-	hst->GetXaxis()->SetTitleSize(0.08);
-	hst->GetYaxis()->SetTitleSize(0.08);
-
+	hst->GetXaxis()->SetLabelSize(0.06);
+	hst->GetYaxis()->SetLabelSize(0.06);
+	hst->GetXaxis()->SetTitleSize(0.06);
+	hst->GetYaxis()->SetTitleSize(0.06);
+	hst->GetYaxis()->SetTitleOffset(1.25);
+//		Do common fit and draw
+	TCanvas *cv = new TCanvas("CV", "R2", 1200, 900);
+	cv->SetLeftMargin(0.15);
+	cv->SetBottomMargin(0.12);
 	hst->Draw();
 	fR2->SetParameter(0, 200000);
 	gr->Fit(fR2);
@@ -49,6 +58,6 @@ void draw_R2(void)
 	TLatex txt;
 	txt.SetTextSize(0.07);
 	sprintf(str, "#chi^{2}/n.d.f. = %6.2f/6", fR2->GetChisquare());
-	txt.DrawLatex(12.3, 2000, str);
+	txt.DrawLatex(11.3, 2000, str);
 }
 
